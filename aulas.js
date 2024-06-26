86b13f305b52430d4ad1568e8debda4de8e08d55
Évelim Dornelles <166746018+evedc@users.noreply.github.com>
2024-06-26 09:19:11 -0300

    Add files via upload

---
+++ aulas.js
@@ -0,0 +1,18 @@
+const nota1 = 5;
+const nota2 = 3;
+const nota3 = 7;
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log(media);
+
+
+if (media < 5) {
+    console.log('Reprovado!!!!');
+}
+
+else if (media >= 5 $$ media <= 7) {
+    console.log('Recuperação');
+}
+else (media > 7) {
+    console.log('Aprovado');
+}
